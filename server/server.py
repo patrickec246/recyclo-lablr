39c0f66228114e4363f595316fe18e52af955f4c
Patrick Caruso <patrickec246@gmail.com>
2019-12-03 20:58:52 -0500

    Update landing page & small server changes

--- server/server.py
+++ server/server.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def render_landing_page():
+    return render_template('landing.html')
+
+@app.route('/label')
+def render_label_page():
+	return render_template('label.html')
+
+@app.route('/donate')
+def render_donate_page():
+	return render_template('donate.html')
+
+@app.route('/about')
+def render_about_page():
+	return render_template('about.html')
 
 @app.route('/request_annotation', methods=['GET'])
 def request_annotation():
@@ -17,6 +29,11 @@ def request_image():
 @app.route('/post_annotation', methods=['GET', 'POST'])
 def post_annotation():
     return ''
+
+@app.route('/trash_data')
+def request_trash_data():
+	return '{"total_trash": 1506, "total_recyclables":1294}'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ server/static/js/landing.js
@@ -0,0 +1,34 @@
+// Credit to Diaco M. Lotfollahi for his beautiful 'Autumn Falling Leaves' with GSAP
+
+var total = 15;
+var container = document.getElementById("pageBackground");
+var w = window.innerWidth;
+var h = window.innerHeight;
+ 
+ for (i=0; i<total; i++){ 
+   var Div = document.createElement('div');
+   TweenLite.set(Div,{attr:{class:'leaf'},x:R(0,w),y:R(-200,-150),z:R(-200,200)});
+   container.appendChild(Div);
+   animm(Div);
+ }
+ 
+ function animm(elm){   
+   TweenMax.to(elm,R(6,15),{y:h+100,ease:Linear.easeNone,repeat:-1,delay:-15});
+   TweenMax.to(elm,R(4,8),{x:'+=100',rotationZ:R(0,180),repeat:-1,yoyo:true,ease:Sine.easeInOut});
+   TweenMax.to(elm,R(2,8),{rotationX:R(0,360),rotationY:R(0,360),repeat:-1,yoyo:true,ease:Sine.easeInOut,delay:-5});
+ };
+
+function R(min,max) {return min+Math.random()*(max-min)};
+
+// Statistics
+function update_trash_statistics() {
+	$.get("/trash_data", function(data) {
+		d = JSON.parse(data);
+		$("#labeledPiecesText").text(d.total_trash);
+		$("#recyclablePiecesText").text(d.total_recyclables);
+	});
+}
+
+setInterval(update_trash_statistics, 3000);
+
+$(document).ready(update_trash_statistics);
